1e22e1b46f3dff7314a85924a44a11121f9caffb
Maciej Kucharek <maciej@slateteams.com>
2022-02-22 12:51:53 +0100

    Initial commit - dummy github action

---
+++ index.js
@@ -0,0 +1,14 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+try {
+    const labelsJsonStr = core.getInput("labels-json-str");
+    console.log(`Hello ${labelsJsonStr}!`);
+    core.setOutput("filtered-labels-json-str", labelsJsonStr);
+
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    console.log(`The event payload: ${payload}`);
+} catch (error) {
+    core.setFailed(error.message);
+}
